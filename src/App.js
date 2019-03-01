442cb36742c1066173dadec2cb467d8ca072e323
Matt McCarley <mdmccarley89@gmail.com>
2019-02-28 22:05:59 -0800

    Begin mocking out the main app page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {/* Header */}
+        <div className="bg-pink-lighter p-4 mb-4">
+          <div className="container mx-auto">
+            {/* Playlistify logo */}
+            <p>playlistify.me</p>
+          </div>
+        </div>
+
+        {/* Content Container */}
+        <div className="container mx-auto">
+          {/* Add Seed Button */}
+          <button className="bg-pink-lighter p-4 rounded-full shadow-md">Add Seed</button>
+          
+          {/* This section is for the full-screen search overlay on add-seed */}
+          {/* <div className="p-4">
+            <h3 className="mb-4">Search</h3>
+            <input className="w-full p-2 rounded shadow-md" type="text"/>
+          </div> */}
+
+          {/* Tunings Section */}
+          <div>
+            <div>Acousticness</div>
+            <div>Danceability</div>
+            <div>Energy</div>
+            <div>Instrumentalness</div>
+            <div>Popularity</div>
+            <div>Loudness</div>
+            <div>Valence</div>
+          </div>
+
+
+        </div>
+
       </div>
     );
   }
