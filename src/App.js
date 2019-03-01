27913a773d136773e9123a48ce192f718af0c9e6
Matt McCarley <mdmccarley89@gmail.com>
2019-02-28 22:44:07 -0800

    More work on building out the mock ui

--- src/App.js
+++ src/App.js
@@ -14,8 +14,52 @@ class App extends Component {
 
         {/* Content Container */}
         <div className="container mx-auto">
-          {/* Add Seed Button */}
-          <button className="bg-pink-lighter p-4 rounded-full shadow-md">Add Seed</button>
+          <div className="flex mb-4">
+            {/* Tunings Section */}
+            <div className="p-4 bg-white rounded-lg shadow-md w-1/4">
+              <h3 className="mb-4">Tunings</h3>
+              <div>
+                <p>acousticness</p>
+                <input className="w-full" type="range" name="acousticness" id=""/>
+              </div>
+              <div>
+                <p>danceability</p>
+                <input className="w-full" type="range" name="danceability" id=""/>
+              </div>
+              <div>
+                <p>energy</p>
+                <input className="w-full" type="range" name="energy" id=""/>
+              </div>
+              <div>
+                <p>instrumentalness</p>
+                <input className="w-full" type="range" name="instrumentalness" id=""/>
+              </div>
+              <div>
+                <p>popularity</p>
+                <input className="w-full" type="range" name="popularity" id=""/>
+              </div>
+              <div>
+                <p>loudness</p>
+                <input className="w-full" type="range" name="loudness" id=""/>
+              </div>
+              <div>
+                <p>valence</p>
+                <input className="w-full" type="range" name="valence" id=""/>
+              </div>
+            </div>
+
+            {/* Seed Container */}
+            <div className="p-4 bg-white rounded-lg shadow-md w-full ml-4 flex flex-col items-center">
+              <h3 className="mb-4">Seeds</h3>
+              {/* Add Seed Button */}
+              <button className="bg-pink-lighter p-2 rounded-full shadow-md flex flex-col justify center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+              </button>
+
+            </div>
+
+          </div>
+          
           
           {/* This section is for the full-screen search overlay on add-seed */}
           {/* <div className="p-4">
@@ -23,15 +67,13 @@ class App extends Component {
             <input className="w-full p-2 rounded shadow-md" type="text"/>
           </div> */}
 
-          {/* Tunings Section */}
-          <div>
-            <div>Acousticness</div>
-            <div>Danceability</div>
-            <div>Energy</div>
-            <div>Instrumentalness</div>
-            <div>Popularity</div>
-            <div>Loudness</div>
-            <div>Valence</div>
+
+
+          
+
+          {/* Results Section */}
+          <div className="p-4 bg-white rounded-lg shadow-md">
+            <h3>Track List</h3>
           </div>
 
 
