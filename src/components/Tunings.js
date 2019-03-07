92de9636e146855e0b1c3c0ff1c69494e540a336
Matt McCarley <mdmccarley89@gmail.com>
2019-03-06 16:07:30 -0800

    work more

--- src/components/Tunings.js
+++ src/components/Tunings.js
@@ -1,95 +1,158 @@
 import React from 'react';
 
-const Tunings = ({handleTuningsAdjustment}) => {
-  return (
-    <div>
-      <h3 className="mb-4 text-center">Tunings</h3>
+class Tunings extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAcousticness: false,
+      isDanceability: false,
+      isEnergy: false,
+      isInstrumentalness: false,
+      isPopularity: false,
+      isLoudness: false,
+      isValence: false,
+    }
+
+    this.handleTuningsToggle = this.handleTuningsToggle.bind(this);
+
+  }
+
+  handleTuningsToggle(event) {
+    const newState = {}
+    const property = `is${event.target.innerText}`;
+    newState[property] = !this.state[property];
+    this.setState(newState);
+  }
+  
+  render() {
+    const { handleTuningsAdjustment } = this.props;
+    
+    return (
       <div>
-        <label htmlFor="acousticness">acousticness</label>
-        <input className="w-full"
-          type="range"
-          name="acousticness"
-          id="acousticness"
-          min="0"
-          max="1"
-          step=".01"
-          onChange={(event) => handleTuningsAdjustment(event, 'acousticness')}
-          />
+        <h3 className="mb-4 text-center">Tunings</h3>
+        <div>
+          <button className="bg-grey-lighter shadow-md rounded p-4 w-full mb-4"
+            onClick={this.handleTuningsToggle}>
+            Acousticness
+          </button>
+          {this.state.isAcousticness && (
+            <input className="w-full mb-6"
+              type="range"
+              name="acousticness"
+              id="acousticness"
+              min="0"
+              max="1"
+              step=".01"
+              onChange={(event) => handleTuningsAdjustment(event, 'acousticness')}
+              />
+          )}
+        </div>
+        <div>
+          <button className="bg-grey-lighter shadow-md rounded p-4 w-full mb-4"
+            onClick={this.handleTuningsToggle}>
+            Danceability
+          </button>
+          {this.state.isDanceability && (
+            <input className="w-full mb-6"
+              type="range"
+              name="danceability"
+              id="danceability"
+              min="0"
+              max="1"
+              step=".01"
+              onChange={(event) => handleTuningsAdjustment(event, 'danceability')}
+              />
+          )}
+        </div>
+        <div>
+          <button className="bg-grey-lighter shadow-md rounded p-4 w-full mb-4"
+            onClick={this.handleTuningsToggle}>
+            Energy
+          </button>
+          {this.state.isEnergy && (
+            <input className="w-full mb-6"
+              type="range"
+              name="energy"
+              id="energy"
+              min="0"
+              max="1"
+              step=".01"
+              onChange={(event) => handleTuningsAdjustment(event, 'energy')}
+              />
+          )}
+        </div>
+        <div>
+          <button className="bg-grey-lighter shadow-md rounded p-4 w-full mb-4"
+            onClick={this.handleTuningsToggle}>
+            Instrumentalness
+          </button>
+          {this.state.isInstrumentalness && (
+            <input className="w-full mb-6"
+            type="range"
+            name="instrumentalness"
+            id="instrumentalness"
+            min="0"
+            max="1"
+            step=".01"
+            onChange={(event) => handleTuningsAdjustment(event, 'instrumentalness')}
+            />
+            )}
+        </div>
+        <div>
+          <button className="bg-grey-lighter shadow-md rounded p-4 w-full mb-4"
+            onClick={this.handleTuningsToggle}>
+            Popularity
+          </button>
+          {this.state.isPopularity && (
+            <input className="w-full mb-6"
+              type="range"
+              name="popularity"
+              id="popularity"
+              min="0"
+              max="100"
+              step="1"
+              onChange={(event) => handleTuningsAdjustment(event, 'popularity')}
+              />
+          )}
+        </div>
+        <div>
+          <button className="bg-grey-lighter shadow-md rounded p-4 w-full mb-4"
+            onClick={this.handleTuningsToggle}>
+            Loudness
+          </button>
+          {this.state.isLoudness && (
+            <input className="w-full mb-6"
+              type="range"
+              name="loudness"
+              id="loudness"
+              min="-60"
+              max="0"
+              step="1"
+              onChange={(event) => handleTuningsAdjustment(event, 'loudness')}
+              />
+          )}
+        </div>
+        <div>
+          <button className="bg-grey-lighter shadow-md rounded p-4 w-full mb-4"
+            onClick={this.handleTuningsToggle}>
+            Valence
+          </button>
+          {this.state.isValence && (
+            <input className="w-full mb-6"
+              type="range"
+              name="valence"
+              id="valence"
+              min="0"
+              max="1"
+              step="0.01"
+              onChange={(event) => handleTuningsAdjustment(event, 'valence')}
+              />
+          )}
+        </div>
       </div>
-      <div>
-        <label htmlFor="danceability">danceability</label>
-        <input className="w-full"
-          type="range"
-          name="danceability"
-          id="danceability"
-          min="0"
-          max="1"
-          step=".01"
-          onChange={(event) => handleTuningsAdjustment(event, 'danceability')}
-          />
-      </div>
-      <div>
-        <label htmlFor="energy">energy</label>
-        <input className="w-full"
-          type="range"
-          name="energy"
-          id="energy"
-          min="0"
-          max="1"
-          step=".01"
-          onChange={(event) => handleTuningsAdjustment(event, 'energy')}
-          />
-      </div>
-      <div>
-        <label htmlFor="instrumentalness">instrumentalness</label>
-        <input className="w-full"
-          type="range"
-          name="instrumentalness"
-          id="instrumentalness"
-          min="0"
-          max="1"
-          step=".01"
-          onChange={(event) => handleTuningsAdjustment(event, 'instrumentalness')}
-          />
-      </div>
-      <div>
-        <label htmlFor="popularity">popularity</label>
-        <input className="w-full"
-          type="range"
-          name="popularity"
-          id="popularity"
-          min="0"
-          max="100"
-          step="1"
-          onChange={(event) => handleTuningsAdjustment(event, 'popularity')}
-          />
-      </div>
-      <div>
-        <label htmlFor="loudness">loudness</label>
-        <input className="w-full"
-          type="range"
-          name="loudness"
-          id="loudness"
-          min="-60"
-          max="0"
-          step="1"
-          onChange={(event) => handleTuningsAdjustment(event, 'loudness')}
-          />
-      </div>
-      <div>
-        <label htmlFor="valence">valence</label>
-        <input className="w-full"
-          type="range"
-          name="valence"
-          id="valence"
-          min="0"
-          max="1"
-          step="0.01"
-          onChange={(event) => handleTuningsAdjustment(event, 'valence')}
-          />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Tunings;
